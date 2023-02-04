7a4338ea3070876dbfc3f706d19549b104b3e99b
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2023-02-04 20:33:01 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>This is inotebook</h1> */}
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route exact path='/about' element={<About/>}></Route>
+          {/* <Route exact path='/' element={<Home/>}></Route> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
